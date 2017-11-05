02f0b7ac5b61ee1eb6bfea3f744316c636918841
MDP <mike@sdpdigital.com>
2017-11-05 09:36:25 -0500

    Added CrfDataProvider unit tests

--- app/src/main/java/org/sagebionetworks/bridge/researchstack/CrfDataProvider.java
+++ app/src/main/java/org/sagebionetworks/bridge/researchstack/CrfDataProvider.java
@@ -1,6 +1,7 @@
 package org.sagebionetworks.bridge.researchstack;
 
 import android.content.Context;
+import android.support.annotation.VisibleForTesting;
 import android.util.Log;
 import android.view.View;
 
@@ -14,12 +15,16 @@ import org.researchstack.backbone.storage.NotificationHelper;
 import org.researchstack.skin.AppPrefs;
 import org.sagebionetworks.bridge.android.manager.BridgeManagerProvider;
 import org.sagebionetworks.bridge.researchstack.wrapper.StorageAccessWrapper;
+import org.sagebionetworks.bridge.rest.model.Message;
 import org.sagebionetworks.bridge.rest.model.ScheduledActivity;
 import org.sagebionetworks.bridge.rest.model.ScheduledActivityListV4;
+import org.sagebionetworks.bridge.rest.model.Study;
 import org.sagebionetworks.bridge.rest.model.StudyParticipant;
+import org.sagebionetworks.bridge.rest.model.UserSessionInfo;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Random;
@@ -27,6 +32,7 @@ import java.util.Random;
 import rx.Completable;
 import rx.Observable;
 import rx.Single;
+import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action0;
 import rx.functions.Action1;
@@ -47,6 +53,12 @@ public class CrfDataProvider extends BridgeDataProvider {
     public CrfDataProvider() {
         // TODO give path to permission file for uploads
         super(BridgeManagerProvider.getInstance());
+    }
+
+    @VisibleForTesting
+    CrfDataProvider(ResearchStackDAO researchStackDAO, StorageAccessWrapper storageAccessWrapper,
+                       TaskHelper taskHelper) {
+        super(researchStackDAO, storageAccessWrapper, taskHelper);
     }
 
     @Override
@@ -76,38 +88,21 @@ public class CrfDataProvider extends BridgeDataProvider {
      */
     public void getCrfActivities(final CrfActivitiesListener listener) {
 
-        if (!CrfPrefs.getInstance().hasFirstSignInDate()) {
-            Log.v(LOG_TAG, "No sign in date detected");
+        if (!getCrfPrefs().hasFirstSignInDate()) {
+            logV("No sign in date detected");
             // getCrfActivities method will be called again when sign in date is found, so return here
             createOrFindFirstSignInDate(listener);
             return;
         }
 
-        DateTime firstSignInDate = CrfPrefs.getInstance().getFirstSignInDate();
-        Log.v(LOG_TAG, String.format(Locale.getDefault(),
+        DateTime firstSignInDate = getCrfPrefs().getFirstSignInDate();
+        logV(String.format(Locale.getDefault(),
                 "Previous sign in date detected %s", firstSignInDate.toString()));
         // We have already done the clinic setup process, and can safely grab the schedules
-        getActivities(firstSignInDate, addTime(firstSignInDate, STUDY_DURATION_IN_DAYS, -1))
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(activityList -> {
-
-                    StringBuilder debugActivityList = new StringBuilder();
-                    for (ScheduledActivity activity : activityList.getItems()) {
-                        if (!activity.getPersistent()) {
-                            if (activity.getActivity().getTask() == null) {
-                                debugActivityList.append(activity.getActivity().getSurvey().getIdentifier());
-                            } else {
-                                debugActivityList.append(activity.getActivity().getTask().getIdentifier());
-                            }
-                            debugActivityList.append(" on ");
-                            debugActivityList.append(activity.getScheduledOn().toString());
-                            debugActivityList.append("\n");
-                        }
-                    }
-                    Log.d(LOG_TAG, debugActivityList.toString());
-
-                    listener.success(activityList);
-                }, throwable -> listener.error(throwable.getLocalizedMessage()));
+        getActivitiesSubscribe(firstSignInDate, endTimeForAllActivities(firstSignInDate), activityList -> {
+            debugPrintActivities(activityList);
+            listener.success(activityList);
+        }, throwable -> listener.error(throwable.getLocalizedMessage()));
     }
 
     /**
@@ -118,33 +113,31 @@ public class CrfDataProvider extends BridgeDataProvider {
      * @param listener the listener for success/fail response
      */
     private void createOrFindFirstSignInDate(final CrfActivitiesListener listener) {
-        Log.v(LOG_TAG, "createOrFindFirstSignInDate");
-        getActivities(DateTime.now(), addTime(DateTime.now(), 1, 0))
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(activityList -> {
+        logV("createOrFindFirstSignInDate");
+        getActivitiesSubscribe(startTime(), endTimeForClinicActivities(), activityList -> {
 
             // We are only interested in the clinic1 and clinic2 activities
             ScheduledActivity clinic1 = findActivity(activityList, CLINIC1);
             ScheduledActivity clinic2 = findActivity(activityList, CLINIC2);
 
             if (clinic1 == null || clinic2 == null) {
-                Log.e(LOG_TAG, "We must have clinic1 or clinic 2 activities to continue, are you in the correct data groups?");
+                logE("We must have clinic1 or clinic 2 activities to continue, are you in the correct data groups?");
                 listener.error("Error: could not find both clinic1 and clinic2, are you in the correct data groups?");
                 return;
             }
 
-            Log.v(LOG_TAG, String.format(Locale.getDefault(),"Clinic1 = %s", clinic1.toString()));
-            Log.v(LOG_TAG, String.format(Locale.getDefault(),"Clinic2 = %s", clinic2.toString()));
+            logV(String.format(Locale.getDefault(),"Clinic1 = %s", clinic1.toString()));
+            logV(String.format(Locale.getDefault(),"Clinic2 = %s", clinic2.toString()));
 
             // Whichever clinic activity is finished is the one this user is a part of
             if (clinic1.getFinishedOn() != null) {  // Found date, go back to loading activities
-                Log.v(LOG_TAG, String.format(Locale.getDefault(),
+                logV(String.format(Locale.getDefault(),
                         "Setting firstSignInDate on clinic1 = %s", clinic1.getFinishedOn().toString()));
-                CrfPrefs.getInstance().setFirstSignInDate(clinic1.getFinishedOn());
+                getCrfPrefs().setFirstSignInDate(clinic1.getFinishedOn());
                 getCrfActivities(listener);
             } else if (clinic2.getFinishedOn() != null) { // Found date, go back to loading activities
-                CrfPrefs.getInstance().setFirstSignInDate(clinic2.getFinishedOn());
-                Log.v(LOG_TAG, String.format(Locale.getDefault(),
+                getCrfPrefs().setFirstSignInDate(clinic2.getFinishedOn());
+                logV(String.format(Locale.getDefault(),
                         "Setting firstSignInDate on clinic1 = %s", clinic2.getFinishedOn().toString()));
                 getCrfActivities(listener);
             } else {
@@ -152,6 +145,13 @@ public class CrfDataProvider extends BridgeDataProvider {
                 findOrCreateClinicGroup(listener, clinic1, clinic2);
             }
         }, throwable -> listener.error(throwable.getLocalizedMessage()));
+    }
+
+    @VisibleForTesting
+    void getActivitiesSubscribe(DateTime start, DateTime end,
+                                final Action1<ScheduledActivityListV4> onNext,
+                                final Action1<Throwable> onError) {
+        getActivities(start, end).observeOn(AndroidSchedulers.mainThread()).subscribe(onNext, onError);
     }
 
     /**
@@ -163,30 +163,34 @@ public class CrfDataProvider extends BridgeDataProvider {
             final CrfActivitiesListener listener,
             ScheduledActivity clinic1, ScheduledActivity clinic2) {
 
-        Log.v(LOG_TAG, "findOrCreateClinicGroup");
+        logV("findOrCreateClinicGroup");
 
         // First let's check if the user has a clinic data group already
         // This check also allows the server to pre-populate clinic groups
-        getStudyParticipant()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(studyParticipant -> {
-                    List<String> dataGroups = studyParticipant.getDataGroups();
+        getStudyParticipantSubscribe(studyParticipant -> {
+            List<String> dataGroups = studyParticipant.getDataGroups();
 
-                    if (dataGroups != null) {
-                        Log.v(LOG_TAG, "dataGroups = " + dataGroups.toString());
-                    }
+            if (dataGroups != null) {
+                logV("dataGroups = " + dataGroups.toString());
+            }
 
-                    if (dataGroups == null ||
-                        dataGroups.isEmpty() ||
-                        (!dataGroups.contains(CLINIC1) || !dataGroups.contains(CLINIC1))) {
-                        assignRandomizedClinic(dataGroups, listener, clinic1, clinic2);
-                    } else {
-                        // We already have the clinic data group assigned, so simply read it and
-                        // trigger the corresponding clinic activity schedule flow
-                        ScheduledActivity clinic = dataGroups.contains(CLINIC1) ? clinic1 : clinic2;
-                        completeClinicSchedule(clinic, listener);
-                    }
-                }, throwable -> listener.error(throwable.getLocalizedMessage()));
+            if (dataGroups == null ||
+                dataGroups.isEmpty() ||
+                (!dataGroups.contains(CLINIC1) || !dataGroups.contains(CLINIC1))) {
+                assignRandomizedClinic(dataGroups, listener, clinic1, clinic2);
+            } else {
+                // We already have the clinic data group assigned, so simply read it and
+                // trigger the corresponding clinic activity schedule flow
+                ScheduledActivity clinic = dataGroups.contains(CLINIC1) ? clinic1 : clinic2;
+                completeClinicSchedule(clinic, listener);
+            }
+        }, throwable -> listener.error(throwable.getLocalizedMessage()));
+    }
+
+    @VisibleForTesting
+    void getStudyParticipantSubscribe(final Action1<StudyParticipant> onNext,
+                                      final Action1<Throwable> onError) {
+        getStudyParticipant().observeOn(AndroidSchedulers.mainThread()).subscribe(onNext, onError);
     }
 
     /**
@@ -199,9 +203,9 @@ public class CrfDataProvider extends BridgeDataProvider {
             List<String> dataGroups, final CrfActivitiesListener listener,
             ScheduledActivity clinic1, ScheduledActivity clinic2) {
 
-        Log.v(LOG_TAG, "assignRandomizedClinic");
+        logV("assignRandomizedClinic");
 
-        final boolean assignToClinic1 = new Random().nextBoolean();
+        final boolean assignToClinic1 = generateRandomClient();
         ScheduledActivity chosenClinic;
         String chosenClinicDataGroup;
         if (assignToClinic1) {
@@ -213,20 +217,28 @@ public class CrfDataProvider extends BridgeDataProvider {
         }
 
         StudyParticipant participant = new StudyParticipant();
-        List<String> newDataGroups = (dataGroups == null) ? new ArrayList<>() : dataGroups;
-        newDataGroups.add(chosenClinicDataGroup);
+        List<String> newDataGroups = Collections.singletonList(chosenClinicDataGroup);
+        if (dataGroups != null) {
+            newDataGroups = new ArrayList<>(dataGroups);
+            newDataGroups.add(chosenClinicDataGroup);
+        }
         participant.setDataGroups(newDataGroups);
 
-        Log.v(LOG_TAG, "Random data group assigned " + chosenClinicDataGroup);
+        logV("Random data group assigned " + chosenClinicDataGroup);
 
-        updateStudyParticipant(participant)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(userSessionInfo -> {
-                    Log.v(LOG_TAG, "Data group successfully assigned");
-                    // We already have the clinic data group assigned, so
-                    // trigger the corresponding clinic activity schedule flow
-                    completeClinicSchedule(chosenClinic, listener);
-                }, throwable -> listener.error(throwable.getLocalizedMessage()));
+        updateStudyParticipantSubscribe(participant, userSessionInfo -> {
+            logV("Data group successfully assigned");
+            // We already have the clinic data group assigned, so
+            // trigger the corresponding clinic activity schedule flow
+            completeClinicSchedule(chosenClinic, listener);
+        }, throwable -> listener.error(throwable.getLocalizedMessage()));
+    }
+
+    @VisibleForTesting
+    void updateStudyParticipantSubscribe(StudyParticipant studyParticipant,
+                                         final Action1<UserSessionInfo> onNext,
+                                         final Action1<Throwable> onError) {
+        updateStudyParticipant(studyParticipant).observeOn(AndroidSchedulers.mainThread()).subscribe(onNext, onError);
     }
 
     /**
@@ -235,25 +247,76 @@ public class CrfDataProvider extends BridgeDataProvider {
      */
     private void completeClinicSchedule(final ScheduledActivity clinic,
                                         final CrfActivitiesListener listener) {
-        Log.v(LOG_TAG, "Completing " + clinic.toString());
+        logV("Completing " + clinic.toString());
 
         // These fields are what is needed to trigger the completion of an activity
-        final DateTime completed = DateTime.now();
+        final DateTime completed = createClinicCompletionDate();
         clinic.setStartedOn(completed);
         clinic.setFinishedOn(completed);
 
-        updateActivity(clinic)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(message -> {
-                    Log.v(LOG_TAG, "Completed clinic successful");
-                    // We have completed the clinic activity which will automatically
-                    // trigger the app's clinic schedules and we can simply pull activities now
-                    CrfPrefs.getInstance().setFirstSignInDate(completed);
-                    getCrfActivities(listener);
-                }, throwable -> listener.error(throwable.getLocalizedMessage()));
+        updateActivitySubscribe(clinic, message -> {
+            logV("Completed clinic successful");
+            // We have completed the clinic activity which will automatically
+            // trigger the app's clinic schedules and we can simply pull activities now
+            getCrfPrefs().setFirstSignInDate(completed);
+            getCrfActivities(listener);
+        }, throwable -> listener.error(throwable.getLocalizedMessage()));
     }
 
-    private ScheduledActivity findActivity(ScheduledActivityListV4 activityList, String identifier) {
+    @VisibleForTesting
+    void updateActivitySubscribe(ScheduledActivity activity,
+                                 final Action1<Message> onNext,
+                                 final Action1<Throwable> onError) {
+        updateActivity(activity).observeOn(AndroidSchedulers.mainThread()).subscribe(onNext, onError);
+    }
+
+    @VisibleForTesting
+    DateTime startTime() {
+        return DateTime.now();
+    }
+
+    @VisibleForTesting
+    DateTime endTimeForClinicActivities() {
+        return addTime(DateTime.now(), 1, 0);
+    }
+
+    @VisibleForTesting
+    DateTime endTimeForAllActivities(DateTime firstSignInDate) {
+        return firstSignInDate.plusDays(STUDY_DURATION_IN_DAYS).minusHours(1);
+    }
+
+    @VisibleForTesting
+    DateTime createClinicCompletionDate() {
+        return DateTime.now();
+    }
+
+    @VisibleForTesting
+    boolean generateRandomClient() {
+        return new Random().nextBoolean();
+    }
+
+    @VisibleForTesting
+    CrfPrefs getCrfPrefs() {
+        return CrfPrefs.getInstance();
+    }
+
+    @VisibleForTesting
+    void logV(String msg) {
+        Log.v(LOG_TAG, msg);
+    }
+
+    @VisibleForTesting
+    void logE(String msg) {
+        Log.e(LOG_TAG, msg);
+    }
+
+    /**
+     * @param activityList to search through
+     * @param identifier of the activity to return
+     * @return activity with identifier, or null if none was found
+     */
+    @VisibleForTesting
+    ScheduledActivity findActivity(ScheduledActivityListV4 activityList, String identifier) {
         if (activityList == null || activityList.getItems() == null || activityList.getItems().isEmpty()) {
             return null;
         }
@@ -277,7 +340,7 @@ public class CrfDataProvider extends BridgeDataProvider {
      * @param days
      * @return
      */
-    private DateTime addTime(DateTime dateTime, int days, int hours) {
+    DateTime addTime(DateTime dateTime, int days, int hours) {
         DateTime newDateTime = dateTime.plusDays(days).plusHours(hours);
 
         // TODO: do this without string manipulation somehow and also add hours/mins based on what was lost
@@ -301,6 +364,24 @@ public class CrfDataProvider extends BridgeDataProvider {
 //        }
 
         return newDateTime;
+    }
+
+    @VisibleForTesting
+    void debugPrintActivities(ScheduledActivityListV4 activityList) {
+        StringBuilder debugActivityList = new StringBuilder();
+        for (ScheduledActivity activity : activityList.getItems()) {
+            if (!activity.getPersistent()) {
+                if (activity.getActivity().getTask() == null) {
+                    debugActivityList.append(activity.getActivity().getSurvey().getIdentifier());
+                } else {
+                    debugActivityList.append(activity.getActivity().getTask().getIdentifier());
+                }
+                debugActivityList.append(" on ");
+                debugActivityList.append(activity.getScheduledOn().toString());
+                debugActivityList.append("\n");
+            }
+        }
+        logV(debugActivityList.toString());
     }
 
     public interface CrfActivitiesListener {
